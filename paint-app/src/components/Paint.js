e9133f9793e7c79f866951f15caaa28c2b1be3b5
Jerry V <jvr572@gmail.com>
2020-06-10 21:06:24 -0400

    custom hook and updated css

--- paint-app/src/components/Paint.js
+++ paint-app/src/components/Paint.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback } from 'react';
+import React, { useState, useEffect, useCallback, useRef } from 'react';
 import randomColor from "randomcolor";
 
 import Name from './Name';
@@ -6,11 +6,21 @@ import Canvas from './Canvas';
 import ColorPicker from './ColorPicker';
 import WindowSize from './WindowSize';
 import RefreshButton from './RefreshButton';
+import useWindowSize from './WindowSize';
 
 
 export default function Paint() {
   const [colors, setColors] = useState([]);
   const [activeColor, setActiveColor] = useState(null);
+  const [visible, setVisible] = useState(false);
+  
+  const timeoutId = useRef();
+
+  const [windowWidth, windowHeight] = useWindowSize(() => {
+    setVisible(true);
+		clearTimeout(timeoutId.current);
+		timeoutId.current = setTimeout(() => setVisible(false), 500);
+  });
 
   const getColors = useCallback(() => {
     const baseColor = randomColor().slice(1);
@@ -37,15 +47,18 @@ export default function Paint() {
 						activeColor={activeColor}
 						setActiveColor={setActiveColor}
 					/>
-          <RefreshButton cb={getColors}/>
+					<RefreshButton cb={getColors} />
 				</div>
 			</header>
 			{activeColor && (
         <Canvas 
           color={activeColor} 
           height={window.innerHeight} 
-        />)}
-			<WindowSize />
+        />
+			)}
+			<div className={`window-size ${visible ? "" : "hidden"}`}>
+				{windowWidth} x {windowHeight}
+			</div>
 		</div>
 	);
 }
