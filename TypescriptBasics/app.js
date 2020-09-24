11da5af0486d33e17ba0507268b70c609d632d37
Jerry V <jvr572@gmail.com>
2020-09-23 21:34:18 -0400

    went over more core types

--- TypescriptBasics/app.js
+++ TypescriptBasics/app.js
@@ -1,5 +1,26 @@
+/*============ */
+/// Objects//////
+/*============ */
+// const person: {
+//  name: string; 
+//  age: number;
+// } = {
+// inference version
 var person = {
     name: "JerrSonJefferson",
-    age: 2000
+    age: 2000,
+    hobbies: ['fitness', 'music']
 };
 console.log(person.name);
+/*============ */
+/// Arrays//////
+/*============ */
+//types
+var favActivities;
+// let favActivities: any[]
+// let favActivities: number[]
+favActivities = ['sports'];
+for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
+    var hobby = _a[_i];
+    console.log(hobby.toUpperCase());
+}
--- TypescriptBasics/app.ts
+++ TypescriptBasics/app.ts
@@ -1,11 +1,62 @@
+/*============ */
+/// Objects//////
+/*============ */
+
 // const person: {
-//  name: string;
+//  name: string; 
 //  age: number;
 // } = {
- // inferenced version
-const person = {
+ // inference version
+const personOBJ = {
  name: "JerrSonJefferson",
- age: 2000
-};
-
+ age: 2000,
+ hobbies: ['fitness', 'music']
+}; 
 console.log(person.name);
+
+/*============ */
+/// Arrays//////
+/*============ */
+
+//types
+let favActivities: string[]
+// let favActivities: any[]
+// let favActivities: number[]
+
+favActivities = ['sports']
+
+for (const hobby  of personOBJ.hobbies) {
+ console.log(hobby.toUpperCase())
+}
+/*============= */
+///// Tuple//////
+/*============= */
+
+
+const personTPL: {
+ name: string; 
+ age: number;
+ hobbies: string[];
+ role: [number, string];
+} = {
+ name: "JerrSonJefferson",
+ age: 2000,
+ hobbies: ['fitness', 'music'],
+ role: [6,'software developer']
+}; 
+
+/*============= */
+///// ENUMS//////
+/*============= */
+
+enum Role {ADMIN, READ_ONLY, AUTHOR}
+
+const personENUM = {
+	name: "JerrSonJefferson",
+	age: 2000,
+	hobbies: ["fitness", "music"],
+	role: Role.ADMIN,
+}; 
+
+
+
