b2294230376e166cd6f43bc6746c97388c78ef8c
Jerry Velazquez <Jvr572@gmail.com>
2021-01-04 19:58:51 -0500

    added git ignore annoying stuff

--- TypescriptBasics/app.js
+++
@@ -1,9 +0,0 @@
-// example of explicitly saying will be the expected output
-function add(n1, n2) {
-    return n1 + n2;
-}
-// Void return type below
-function printResult(num) {
-    console.log("Result:  " + num);
-}
-printResult(add(5, 5));
--- TypescriptBasics/basics.ts
+++
@@ -1,5 +0,0 @@
-function addition(n1: number, n2: number) {
- return n1 + n2
-}
-
-console.log(addition(3, 2))
