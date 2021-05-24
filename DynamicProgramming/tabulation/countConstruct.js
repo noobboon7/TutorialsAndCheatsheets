d816c22c610bdab1c5136109d042fb7267753b4d
Jerry Velazquez <Jvr572@gmail.com>
2021-05-24 13:26:19 -0400

    done with count tabulation

--- DynamicProgramming/tabulation/countConstruct.js
+++ DynamicProgramming/tabulation/countConstruct.js
@@ -7,8 +7,18 @@ You may reuse elements of wordBank as many times as needed.
 */
 
 const countConstruct = (target, wordBank) => {
-  const table = Array(target.length+1);
+  const table = Array(target.length+1).fill(0);
   table[0] = 1; 
+
+  for (let i = 0; i < target.length; i++) {
+    for (const word of wordBank) {
+      if (target.slice(i, i+word.length) === word) {
+        table[i+word.length] += table[i];
+      }
+    }
+  }
+
+  return table[target.length];
 };
 
 console.log(countConstruct("purple", ["purp", "p", "ur", "le", "purpl"]));
